b36bf21b20202bdd10b5f2f494baf7c788ba3942
np31-Nik <nik.trabajos@gmail.com>
2022-12-17 16:08:49 +0100

    Añadir programa inicial

---
+++ programa.py
@@ -0,0 +1,17 @@
+print("Hola, elige una opcion:")
+print("1. Sumar")
+print("2. Restar")
+print("3. dividir")
+i= int(input("elige:"))
+
+if(i==1):
+    print("Sumando")
+elif(i==2):
+    print("Restando")
+elif(i==3):
+    print("dividiendo")
+else:
+    print("Opcion incorrecta")
+
+
+print("Adios")
